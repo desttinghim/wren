866e78e8d6a93687b1330a6ea78fa561f84da882
Louis Pearson <opensource@louispearson.work>
2023-02-22 13:40:20 -0700

    Improve zig bindings

--- projects/zig/src/main.zig
+++ projects/zig/src/main.zig
@@ -2,153 +2,55 @@ const std = @import("std");
 const testing = std.testing;
 const c = @import("c.zig");
 
-const WrenCtx = @This();
+const Wren = @This();
 
-var wren_contexts_global: ?std.AutoHashMap(*c.WrenVM, WrenCtx) = undefined;
+config: c.WrenConfiguration = undefined,
 
-log: ?*std.ArrayList(u8),
-log_to_stdout: bool,
-
-methods: ?*std.StringHashMap(c.WrenForeignMethodFn),
-classes: ?*std.StringHashMap(c.WrenForeignClassMethods),
-
-const WrenContextOptions = struct {
-    log_to_std: bool = false,
+const Options = struct {
+    writeFn: ?c.WrenWriteFn,
+    errorFn: ?c.WrenErrorFn,
+    bindForeignClassFn: ?c.WrenBindForeignClassFn,
+    bindForeignMethodFn: ?c.WrenBindForeignMethodFn,
 };
 
-fn WrenContext(comptime options: WrenContextOptions) type {
-    return struct {
-        var config: c.WrenConfiguration = undefined;
-        var log_allocator: ?std.mem.Allocator = null;
-        var log = std.ArrayListUnmanaged(u8){};
-        var data_allocator: ?std.mem.Allocator = null;
-        var methods = std.StringHashMapUnmanaged(c.WrenForeignMethodFn){};
-        var classes = std.StringHashMapUnmanaged(c.WrenForeignClassMethods){};
-
-        pub fn init_config() void {
-            c.wrenInitConfiguration(&config);
-            config.writeFn = writeFn;
-            config.errorFn = errorFn;
-            config.bindForeignClassFn = bindForeignClass;
-            config.bindForeignMethodFn = bindForeignMethod;
-        }
-        pub fn init_vm() !*c.WrenVM {
-            return c.wrenNewVM(&config) orelse return error.NullVM;
-        }
-        pub fn deinit_vm(vm: *c.WrenVM) void {
-            c.wrenFreeVM(vm);
-        }
-        pub fn deinit() void {
-            if (log_allocator) |allocator| {
-                log.clearAndFree(allocator);
-            }
-        }
-        fn _log(text: [*:0]const u8) void {
-            if (options.log_to_std) {
-                std.debug.print("{s}", .{text});
-            }
-
-            if (log_allocator) |allocator| {
-                const text_span = std.mem.span(text);
-                _ = log.writer(allocator).write(text_span) catch {};
-            }
-        }
-
-        // Binding functions
-        pub fn writeFn(vm_opt: ?*c.WrenVM, text_opt: ?[*:0]const u8) callconv(.C) void {
-            _ = vm_opt;
-            const text = text_opt orelse "null";
-            _log(text);
-        }
-        pub fn errorFn(
-            vm_opt: ?*c.WrenVM,
-            errorType: c.WrenErrorType,
-            module_opt: ?[*:0]const u8,
-            line: c_int,
-            msg_opt: ?[*:0]const u8,
-        ) callconv(.C) void {
-            _ = msg_opt;
-            _ = line;
-            _ = module_opt;
-            _ = errorType;
-            _ = vm_opt;
-            // const vm = vm_opt orelse return;
-            // const msg = msg_opt orelse return;
-            // const module = module_opt orelse return;
-            // switch (errorType) {
-            //     c.WREN_ERROR_COMPILE => _log(std.fmt.format("[{s} line {}] [Error] {s}\n", .{ module, line, msg }) catch return),
-            //     c.WREN_ERROR_STACK_TRACE => std.debug.print("[{s} line {}] in {s}\n", .{ module, line, msg }),
-            //     c.WREN_ERROR_RUNTIME => std.debug.print("[Runtime Error] {s}\n", .{msg}),
-            //     else => std.debug.print("[Unexpected Error] {s}", .{msg}),
-            // }
-        }
-        pub fn bindForeignMethod(
-            vm_opt: ?*c.WrenVM,
-            module_opt: ?[*:0]const u8,
-            class_name_opt: ?[*:0]const u8,
-            is_static: bool,
-            signature_opt: ?[*:0]const u8,
-        ) callconv(.C) c.WrenForeignMethodFn {
-            _ = vm_opt;
-            const module = module_opt orelse @panic("Passed null module");
-            const class_name = class_name_opt orelse @panic("Passed null class_name");
-            const signature = signature_opt orelse @panic("Passed null signature");
-
-            var buffer: [4096]u8 = undefined;
-
-            const name = std.fmt.bufPrintZ(&buffer, "{s}/{s}.{s}{s}", .{ module, class_name, signature, if (is_static) "static" else "" }) catch return null;
-
-            std.debug.print("Looking for method: {s}\n", .{name});
-
-            return methods.get(name) orelse null;
-        }
-
-        pub fn bindForeignClass(
-            vm_opt: ?*c.WrenVM,
-            module_opt: ?[*:0]const u8,
-            class_name_opt: ?[*:0]const u8,
-        ) callconv(.C) c.WrenForeignClassMethods {
-            _ = vm_opt;
-            const null_class: c.WrenForeignClassMethods = .{
-                .allocate = null,
-                .finalize = null,
-            };
-
-            const module = module_opt orelse @panic("Passed null module");
-            const class_name = class_name_opt orelse @panic("Passed null class_name");
-
-            var buffer: [4096]u8 = undefined;
-
-            const name = std.fmt.bufPrintZ(&buffer, "{s}/{s}", .{ module, class_name }) catch return null_class;
-
-            std.debug.print("Looking for class: {s}\n", .{name});
-
-            return classes.get(name) orelse null_class;
-        }
-    };
+pub fn init(opt: Options) Wren {
+    var self = Wren{};
+    c.wrenInitConfiguration(&self.config);
+    // Configure wren
+    if (opt.writeFn) |writeFn| self.config.writeFn = writeFn;
+    if (opt.errorFn) |errorFn| self.config.errorFn = errorFn;
+    if (opt.bindForeignClassFn) |bindForeignClassFn| self.config.bindForeignClassFn = bindForeignClassFn;
+    if (opt.bindForeignMethodFn) |bindForeignMethodFn| self.config.bindForeignMethodFn = bindForeignMethodFn;
+    return self;
 }
 
-test WrenContext {
-    const Wren = WrenContext(.{});
-    defer Wren.deinit();
-
-    Wren.init_config();
-
-    Wren.log_allocator = testing.allocator;
-    Wren.data_allocator = testing.allocator;
-
-    var vm = try Wren.init_vm();
-    defer Wren.deinit_vm(vm);
-
-    const module = "main";
-    const script =
-        \\System.print("I am running in a VM!")
-    ;
-
-    try handle_result(c.wrenInterpret(vm, module, script));
-
-    try testing.expectEqualStrings("I am running in a VM!\n", Wren.log.items);
+pub fn new(self: *Wren) !*VM {
+    var vm: *c.WrenVM = c.wrenNewVM(&self.config) orelse return error.NullVM;
+    return @ptrCast(*VM, vm);
 }
+
+const VM = opaque {
+    /// Casts anyopaque to self pointer
+    pub fn from_anyopaque(ptr_opt: ?*anyopaque) *@This() {
+        const ptr = ptr_opt orelse @panic("Passed null ptr");
+        return @ptrCast(*@This(), @constCast(@alignCast(@alignOf(@This()), ptr)));
+    }
+    fn as_raw(vm: *VM) *c.WrenVM {
+        return @ptrCast(*c.WrenVM, @constCast(@alignCast(@alignOf(@This()), vm)));
+    }
+    pub fn deinit(vm: *VM) void {
+        c.wrenFreeVM(vm.as_raw());
+    }
+    pub fn interpret(vm: *VM, module: [*:0]const u8, script: [*:0]const u8) !void {
+        try handle_result(c.wrenInterpret(vm.as_raw(), module, script));
+    }
+    pub fn setUserData(vm: *VM, user_data: *anyopaque) void {
+        c.wrenSetUserData(vm.as_raw(), user_data);
+    }
+    pub fn getUserData(vm: *VM) ?*anyopaque {
+        return c.wrenGetUserData(vm.as_raw());
+    }
+};
 
 pub fn handle_result(result: c.WrenInterpretResult) !void {
     switch (result) {
@@ -159,33 +61,131 @@ pub fn handle_result(result: c.WrenInterpretResult) !void {
     }
 }
 
-// test "init wren vm" {
-//     var log = std.ArrayList(u8).init(testing.allocator);
-//     defer log.deinit();
+const TestHarness = struct {
+    vm: *VM = undefined,
+    log: std.ArrayListUnmanaged(u8) = .{},
+    methods: std.StringHashMapUnmanaged(c.WrenForeignMethodFn) = .{},
+    classes: std.StringHashMapUnmanaged(c.WrenForeignClassMethods) = .{},
 
-//     var config: c.WrenConfiguration = undefined;
-//     c.wrenInitConfiguration(&config);
-//     {
-//         // Configure wren
-//         config.writeFn = writeFn;
-//         config.errorFn = errorFn;
-//     }
+    /// Casts anyopaque to self pointer
+    fn from(ptr_opt: ?*anyopaque) *@This() {
+        const ptr = ptr_opt orelse @panic("Passed null ptr");
+        return @ptrCast(*@This(), @constCast(@alignCast(@alignOf(@This()), ptr)));
+    }
 
-//     var vm: *c.WrenVM = c.wrenNewVM(&config) orelse return error.NullVM;
-//     defer c.wrenFreeVM(vm);
+    /// Creates a new vm on the stack
+    fn init(harness: *@This()) !void {
+        var config = Wren.init(.{
+            .writeFn = writeFn,
+            .errorFn = errorFn,
+            .bindForeignClassFn = bindForeignClass,
+            .bindForeignMethodFn = bindForeignMethod,
+        });
+        harness.vm = try config.new();
+        harness.vm.setUserData(harness);
+    }
 
-//     try register_context(vm, .{ .log_list = &log });
-//     defer free_all_contexts();
+    pub fn deinit(self: *@This()) void {
+        self.vm.deinit();
+        self.log.clearAndFree(testing.allocator);
+        self.methods.clearAndFree(testing.allocator);
+        self.classes.clearAndFree(testing.allocator);
+    }
 
-//     const module = "main";
-//     const script =
-//         \\System.print("I am running in a VM!")
-//     ;
+    fn writeFn(vm_opt: ?*c.WrenVM, text_opt: ?[*:0]const u8) callconv(.C) void {
+        const vm = VM.from_anyopaque(vm_opt);
+        const self = from(vm.getUserData());
+        const text = text_opt orelse @panic("null string");
+        const writer = self.log.writer(testing.allocator);
+        std.fmt.format(writer, "{s}", .{text}) catch @panic("Error formatting write");
+    }
 
-//     try handle_result(c.wrenInterpret(vm, module, script));
+    fn errorFn(
+        vm_opt: ?*c.WrenVM,
+        errorType: c.WrenErrorType,
+        module_opt: ?[*:0]const u8,
+        line: c_int,
+        msg_opt: ?[*:0]const u8,
+    ) callconv(.C) void {
+        const vm = VM.from_anyopaque(vm_opt);
+        const module = module_opt orelse @panic("null msg");
+        const msg = msg_opt orelse @panic("null msg");
 
-//     try testing.expectEqualStrings("I am running in a VM!\n", log.items);
-// }
+        const self = from(vm.getUserData());
+        const writer = self.log.writer(testing.allocator);
+
+        _ = switch (errorType) {
+            c.WREN_ERROR_COMPILE => std.fmt.format(writer, "[{s} line {}] [Error] {s}\n", .{ module, line, msg }),
+            c.WREN_ERROR_STACK_TRACE => std.fmt.format(writer, "[{s} line {}] in {s}\n", .{ module, line, msg }),
+            c.WREN_ERROR_RUNTIME => std.fmt.format(writer, "[Runtime Error] {s}\n", .{msg}),
+            else => std.fmt.format(writer, "[Unexpected Error] {s}", .{msg}),
+        } catch @panic("Error formatting error");
+    }
+    fn bindForeignMethod(
+        vm_opt: ?*c.WrenVM,
+        module_opt: ?[*:0]const u8,
+        class_name_opt: ?[*:0]const u8,
+        is_static: bool,
+        signature_opt: ?[*:0]const u8,
+    ) callconv(.C) c.WrenForeignMethodFn {
+        const vm = VM.from_anyopaque(vm_opt);
+        const module = module_opt orelse @panic("Passed null module");
+        const class_name = class_name_opt orelse @panic("Passed null class_name");
+        const signature = signature_opt orelse @panic("Passed null signature");
+
+        const self = from(vm.getUserData());
+        const writer = self.log.writer(testing.allocator);
+
+        var buffer: [4096]u8 = undefined;
+
+        const name = std.fmt.bufPrintZ(&buffer, "{s}/{s}.{s}{s}", .{ module, class_name, signature, if (is_static) "static" else "" }) catch return null;
+
+        std.fmt.format(writer, "Looking for method: {s}\n", .{name}) catch @panic("Error logging");
+
+        return self.methods.get(name) orelse null;
+    }
+
+    const null_class: c.WrenForeignClassMethods = .{
+        .allocate = null,
+        .finalize = null,
+    };
+
+    fn bindForeignClass(
+        vm_opt: ?*c.WrenVM,
+        module_opt: ?[*:0]const u8,
+        class_name_opt: ?[*:0]const u8,
+    ) callconv(.C) c.WrenForeignClassMethods {
+        const vm = VM.from_anyopaque(vm_opt);
+        const module = module_opt orelse @panic("Passed null module");
+        const class_name = class_name_opt orelse @panic("Passed null class_name");
+
+        const self = from(vm.getUserData());
+        const writer = self.log.writer(testing.allocator);
+
+        var buffer: [4096]u8 = undefined;
+
+        const name = std.fmt.bufPrintZ(&buffer, "{s}/{s}", .{ module, class_name }) catch return null_class;
+
+        std.fmt.format(writer, "Looking for class: {s}\n", .{name}) catch @panic("Error logging");
+
+        return self.classes.get(name) orelse null_class;
+    }
+};
+
+test "init wren vm" {
+    var harness = TestHarness{};
+    try harness.init();
+    defer harness.deinit();
+
+    const module = "main";
+    const script =
+        \\System.print("I am running in a VM!")
+    ;
+
+    try harness.vm.interpret(module, script);
+
+    try testing.expectEqualStrings("I am running in a VM!\n", harness.log.items);
+}
 
 // test "call static method" {
 //     var log = std.ArrayList(u8).init(testing.allocator);
